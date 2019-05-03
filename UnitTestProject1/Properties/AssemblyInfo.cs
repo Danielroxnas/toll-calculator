0fc0eef54ccd213727b7535b8ddb70809deccc9a
daniel.roxnas <daniel.roxnas@barium.se>
2019-05-03 19:38:35 +0200

    Refactor and test added

--- UnitTestProject1/Properties/AssemblyInfo.cs
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -2,15 +2,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("TollCalculatorTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("TollCalculatorTests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("fd1a9256-2770-4274-8b1a-3f7c9dcdbd26")]
